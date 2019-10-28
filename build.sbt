7e6f574993515e2bcf635fcbe9a894b53cdcd9e0
deepakkumarlakshmipathi <Deepak.lakshmipathi550@gmail.com>
2019-10-28 12:22:53 -0400

    Initial Commit:

--- build.sbt
+++ build.sbt
@@ -2,12 +2,11 @@ name := "Scala for the impatient"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.12.0"
 
 
-libraryDependencies += "org.apache.spark" % "spark-core" % "2.4.4"
-libraryDependencies += "org.apache.spark" % "spark-sql" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4"
 
 // spark-hive
-libraryDependencies += "org.apache.spark" % "spark-hive" % "2.4.4" % "provided"
-
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.4.4" % "provided"
--- src/main/scala/Cars_tab_filter.scala
+++ src/main/scala/Cars_tab_filter.scala
@@ -1,21 +1,19 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+
 class Cars_tab_filter {
   def main(args: Array[String]): Unit = {
-    import org.apache.spark.sql.SQLContext
-    import scala.collection.mutable.ArrayBuffer
-    import org.apache.spark.sql.functions._
 
-
-    val file = spark.read.format("csv").option("header","true").option('quote', "\"").option("escape", "\"").option("multiLine","true").option("inferSchema","true").load("s3://s3-test-550-2019/craigslistcarsFull.csv")
-
-
+    val spark = SparkSession.builder().appName("test").enableHiveSupport().getOrCreate()
+    val sc = spark.sparkContext
+    val file = spark.read.format("csv").option("header","true").option("quote", "\"").option("escape", "\"").option("multiLine","true").option("inferSchema","true").load("s3://s3-test-550-2019/craigslistcarsFull.csv")
     val sqlC = new org.apache.spark.sql.SQLContext(sc)
     val temp_v = file.createOrReplaceTempView("File")
     val tab_1 = sqlC.sql("SELECT city,price,year,manufacturer,Make,odometer,type,paint_color from File")
-    val temp_v = file.createOrReplaceTempView("File")
-    val tab_1 = sqlC.sql("SELECT city,price,year,manufacturer,Make,odometer,type,paint_color from File")
     val tab2 = tab_1.na.drop(how= "any")
-    val tab3 = tab2.withColumn("id",monotonicallyIncreasingId)
-    val temp_v = tab3.createOrReplaceTempView("tab3")
+    val tab3 = tab2.withColumn("id",monotonically_increasing_id())
+    val temp_v1 = tab3.createOrReplaceTempView("tab3")
     val tab4 = sqlC.sql("SELECT id, city, price, year, CONCAT(manufacturer,' ',make) Cars, odometer, type, paint_color AS Color from tab3")
     tab4.write.format("csv").save("s3://s3-test-550-2019/filtered_cars_data")
 
